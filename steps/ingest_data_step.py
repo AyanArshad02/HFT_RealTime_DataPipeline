4cfa88821a69eb3ef011abd2827e3498589deeb1
Md Ayan Arshad <ayanarshad2002@gmail.com>
2024-11-17 23:34:39 +0530

    Added ingest_data_step.py

---
+++ steps/ingest_data_step.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from zenml.steps import step
+
+from src.ingest_data import DataIngestor
+
+def ingest_data_step(ticker_symbol: str) -> 'pd.DataFrame':
+    """
+    Ingests stock data for a given ticker symbol using the DataIngestor class.
+
+    This function retrieves historical stock data for the specified ticker symbol 
+    by leveraging the `DataIngestor` class and returns the data as a pandas DataFrame.
+
+    Parameters:
+        ticker_symbol (str): The ticker symbol of the stock to ingest data for (e.g. 'TSLA' for Tesla).
+
+    Returns:
+        pd.DataFrame: A pandas DataFrame containing the ingested stock data.
+    """
+    
+    ingestor = DataIngestor()
+    stock_data = ingestor.ingest_data(ticker_symbol)
+    return stock_data
