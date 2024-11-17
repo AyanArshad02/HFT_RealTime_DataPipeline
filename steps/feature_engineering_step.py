69fbd31f7a6494419765accbfe199d967eaf27b5
Md Ayan Arshad <ayanarshad2002@gmail.com>
2024-11-18 00:05:13 +0530

    Added feature_engineering_step.py

---
+++ steps/feature_engineering_step.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from zenml.steps import step
+
+from src.feature_engineering import FeatureEngineer
+
+@step
+def feature_engineering_step(stock_data) -> 'pd.DataFrame':
+    """
+    Performs feature engineering on the provided stock data using the FeatureEngineer class.
+
+    This ZenML step applies feature engineering techniques to enhance the given stock data. 
+
+    Parameters:
+        stock_data (pd.DataFrame): A pandas DataFrame containing the stock data to process.
+
+    Returns:
+        pd.DataFrame: A pandas DataFrame with engineered features added or modified, 
+        prepared for further analysis or modeling.
+    """
+    engineer = FeatureEngineer()
+    return engineer.engineer(stock_data)
