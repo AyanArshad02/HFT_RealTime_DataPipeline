93e26a3940242470670213bae2118e6458c6763e
Md Ayan Arshad <ayanarshad2002@gmail.com>
2024-11-19 14:37:28 +0530

    added pipeline

--- pipelines/run_pipeline.py
+++ pipelines/run_pipeline.py
@@ -1,9 +1,21 @@
+import sys
+sys.path.append('/Users/mdayanarshad/Desktop/Data_Science_Projects/HFT_RealTime_DataPipeline')
+
+
 from zenml.pipelines import pipeline
+from zenml.steps import step
 from steps import ingest_data_step, handle_missing_value_step, feature_engineering_step, storing_preprocessed_data_step
 
+ticker_symbol = "AAPL"
+
 @pipeline
-def run_pipeline(ticker_symbol: str):
-    stock_data = ingest_data_step(ticker_symbol=ticker_symbol)
+def run_pipeline():
+    stock_data = ingest_data_step(ticker_symbol="AAPL")
     stock_data = handle_missing_value_step(stock_data)
     stock_data = feature_engineering_step(stock_data)
     storing_preprocessed_data_step(stock_data, ticker_symbol)
+
+# run_pipeline = pipeline()(run_pipeline)
+
+if __name__ == "__main__":
+    run_pipeline()
